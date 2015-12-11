ea8a030372bd7f195cd76712f9b14bb30743d2e4
Maik Riechert <maik.riechert@arcor.de>
2015-12-11 22:47:11 +0000

    replace uint8 DataView reads by typed array access
    With this change, there are speed increases of 15% on Chrome 47 and 36% on Firefox 42 while decoding around 200MiB of data. Testing was done with a very large array of numbers where one third of elements were nulls.

--- cbor.js
+++ cbor.js
@@ -185,6 +185,7 @@ function encode(value) {
 
 function decode(data, tagger, simpleValue) {
   var dataView = new DataView(data);
+  var ta = new Uint8Array(data);
   var offset = 0;
   
   if (typeof tagger !== "function")
@@ -225,7 +226,7 @@ function decode(data, tagger, simpleValue) {
     return read(dataView.getFloat64(offset), 8);
   }
   function readUint8() {
-    return read(dataView.getUint8(offset), 1);
+    return read(ta[offset], 1);
   }
   function readUint16() {
     return read(dataView.getUint16(offset), 2);
@@ -237,7 +238,7 @@ function decode(data, tagger, simpleValue) {
     return readUint32() * POW_2_32 + readUint32();
   }
   function readBreak() {
-    if (dataView.getUint8(offset) !== 0xff)
+    if (ta[offset] !== 0xff)
       return false;
     offset += 1;
     return true;
