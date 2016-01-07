61c87c8a23209dbe7c1991da1fd2382a849346d8
Maik Riechert <maik.riechert@arcor.de>
2016-01-07 13:48:46 +0100

    Support nodejs (module.exports)

--- cbor.js
+++ cbor.js
@@ -418,6 +418,8 @@ var obj = { encode: encode, decode: decode };
 
 if (typeof define === "function" && define.amd)
   define("cbor/cbor", obj);
+else if (typeof module !== "undefined" && module.exports)
+  module.exports = obj;
 else if (!global.CBOR)
   global.CBOR = obj;
 
