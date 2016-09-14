b2e1f45c49ad1f23ee0dd102eb012767ff751966
Patrick Gansterer <paroga@paroga.com>
2016-09-14 17:48:48 +0200

    Update dependencies and copyright year and bump version

--- cbor.js
+++ cbor.js
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 Patrick Gansterer <paroga@paroga.com>
+ * Copyright (c) 2014-2016 Patrick Gansterer <paroga@paroga.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
